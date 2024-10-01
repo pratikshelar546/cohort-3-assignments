2a9d725c677d8d3d35a4738c1c426fa3cbad36e1
pratikshelar546 <66733074+pratikshelar546@users.noreply.github.com>
2024-10-02 02:23:58 +0530

    jwt verify added

--- week-6/week-6-authentication/index.js
+++ week-6/week-6-authentication/index.js
@@ -1,20 +1,21 @@
 const express = require("express");
-
+const JWT_SECRET = "lifehack";
+const jwt = require("jsonwebtoken");
 const app = express();
 
 app.use(express.json());
 
 const users = [];
 
-function genrateToken() {
-    let token = '';
-    let options = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+// function genrateToken() {
+//     let token = '';
+//     let options = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
 
-    for (let i = 0; i < 32; i++) {
-        token += options[Math.floor(Math.random() * options.length)]
-    }
-    return token
-}
+//     for (let i = 0; i < 32; i++) {
+//         token += options[Math.floor(Math.random() * options.length)]
+//     }
+//     return token
+// }
 
 
 
@@ -39,8 +40,11 @@ app.post("/signin", (req, res) => {
     let user = users.find(user => user.userName == userName && user.password == password);
 
     if (user) {
-        const token = genrateToken();
-        user.token = token;
+        // const token = genrateToken();
+        const token = jwt.sign({
+            userName: userName
+        }, JWT_SECRET)
+
         res.json({ token })
     } else {
         res.status(404).json({ message: "User not found" });
@@ -54,7 +58,9 @@ app.post("/signin", (req, res) => {
 
 app.get("/me", (req, res) => {
     const token = req.headers.token;
-    const user = users.find(user => user.token == token);
+    const decodedData = jwt.verify(token, JWT_SECRET);
+    const userName = decodedData.userName;
+    const user = users.find(user => user.userName == userName);
 
     if (user) {
         res.json({ user })
