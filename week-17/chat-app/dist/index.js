185d585fafc7c06d9094481881af299cd9e36bf1
pratikshelar546 <66733074+pratikshelar546@users.noreply.github.com>
2024-12-05 00:14:06 +0530

    chat app backend done

--- week-17/chat-app/dist/index.js
+++ week-17/chat-app/dist/index.js
@@ -2,17 +2,38 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const ws_1 = require("ws");
 const wss = new ws_1.WebSocketServer({ port: 8080 });
-let userCount = 0;
 let allSocket = [];
+// const message = {
+//   type: "join",
+//   payload: {
+//     roomId: "red",
+//   },
+// };
+// const message = {
+//   type: "chat",
+//   payload: {
+//     message: "hello",
+//   },
+// };
 wss.on("connection", function (socket) {
-    userCount = userCount + 1;
-    allSocket.push(socket);
-    console.log("connected successfully");
     socket.on("message", (message) => {
-        allSocket.forEach((socket, index) => socket.send(`${message} from user ${index + 1}`));
-        // socket.send(`${message} from user ${userCount}`);
-    });
-    socket.on("error", (error) => {
-        console.log(error);
+        var _a;
+        // @ts-ignore
+        const parseMessage = JSON.parse(message);
+        if (parseMessage.type == "join") {
+            allSocket.push({
+                socket: socket,
+                room: parseMessage.payload.room,
+            });
+        }
+        if (parseMessage.type == "chat") {
+            const currentUserRoom = (_a = allSocket.find((x) => x.socket == socket)) === null || _a === void 0 ? void 0 : _a.room;
+            allSocket.map((user) => {
+                if (user.room == currentUserRoom) {
+                    console.log("true");
+                    user.socket.send(parseMessage.payload.message);
+                }
+            });
+        }
     });
 });
--- week-17/chat-app/src/index.ts
+++ week-17/chat-app/src/index.ts
@@ -1,20 +1,49 @@
-import { WebSocketServer,WebSocket } from "ws";
+import { WebSocketServer, WebSocket } from "ws";
 
 const wss = new WebSocketServer({ port: 8080 });
 
-let userCount = 0;
-let allSocket : WebSocket[] = [];
+interface Users {
+  socket: WebSocket;
+  room: string;
+}
+let allSocket: Users[] = [];
+
+// const message = {
+//   type: "join",
+//   payload: {
+//     roomId: "red",
+//   },
+// };
+
+// const message = {
+//   type: "chat",
+//   payload: {
+//     message: "hello",
+//   },
+// };
+
 wss.on("connection", function (socket) {
-  userCount = userCount + 1;
-  allSocket.push(socket);
-  console.log("connected successfully");
   socket.on("message", (message) => {
-    allSocket.forEach((socket,index) =>
-      socket.send(`${message} from user ${index+1}`)
-    );
-    // socket.send(`${message} from user ${userCount}`);
-  });
-  socket.on("error", (error) => {
-    console.log(error);
+    // @ts-ignore
+    const parseMessage = JSON.parse(message);
+
+    if (parseMessage.type == "join") {
+      allSocket.push({
+        socket: socket,
+        room: parseMessage.payload.room,
+      });
+    }
+
+    if (parseMessage.type == "chat") {
+      const currentUserRoom = allSocket.find((x) => x.socket == socket)?.room;
+
+      allSocket.map((user) => {
+        if (user.room == currentUserRoom) {
+          console.log("true");
+
+          user.socket.send(parseMessage.payload.message);
+        }
+      });
+    }
   });
 });
